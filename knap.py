f3afb409a326fcd41329a4cbc8895ae4a748850f
Jenish Modh <70342788+jenishmodh@users.noreply.github.com>
2024-01-17 17:11:59 +0530

    lamprostechTest

--- knap.py
+++ knap.py
@@ -1,12 +1,12 @@
-weights = [3, 1, 4]
-values = [4, 4, 1]
-capacity = 5
+weights = list(map(int, input("Enter the weights: ").split()))
+values = list(map(int, input("Enter the values: ").split()))
+weight_limit = int(input("Enter the weight limit: "))
 
 n = len(weights)
-table = [[0 for x in range(capacity + 1)] for x in range(n + 1)]
+table = [[0 for x in range(weight_limit + 1)] for x in range(n + 1)]
 
 for i in range(1, n + 1):
-    for j in range(1, capacity + 1):
+    for j in range(1, weight_limit + 1):
         if weights[i - 1] <= j:
             table[i][j] = max(
                 values[i - 1] + table[i - 1][j - weights[i - 1]], table[i - 1][j]
@@ -14,4 +14,4 @@ for i in range(1, n + 1):
         else:
             table[i][j] = table[i - 1][j]
 
-print(table[n][capacity])
+print("Maximum value achievable:", table[n][weight_limit])
